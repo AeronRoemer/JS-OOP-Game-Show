18a8baa632f633c72081863ddf701e86bb023585
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-02-03 23:04:35 -0700

    minor JS changes day 1

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,19 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ // creates class Game
+class Game{
+    constructor(){
+      this.missed = 0,
+      this.phrases = ['eagle eye', 'monkeys at midnight', 'loose knit socks', 'left head of cerberus', 'big knife brawl'],
+      this.activePhrase = null
+    }   
+// returns one of five predefined phrases
+    getRandomPhrase(){
+        return this.phrases[Math.floor(Math.random() * 5)];
+    }
+   }
+
+const game = new Game;
+console.log(game.getRandomPhrase())
